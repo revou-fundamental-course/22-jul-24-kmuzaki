2672b70dd47da913c6c7526b273da4b40bc3df68
Kmuzaki <keihanmuzaki0480@gmail.com>
2024-07-30 21:13:00 +0800

    Menambah bannerslide

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-ini Javascript
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var imglist = document.getElementsByClassName("main-content-banner");
+    if (n > imglist.length) slideIndex = 1;
+    else if(n < 1) slideIndex = imglist.length;
+
+    for(i = 0; i < imglist.length; i++) {
+        imglist[i].style.display = 'none';
+    }
+    
+    imglist[slideIndex -1].style.display = 'block';
+}    
+
+setInterval(() => {
+    plusDivs(1);
+}, 1000);
